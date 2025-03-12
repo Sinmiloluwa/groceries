793879bdcd4eb041054bbf18fa79bae47b6e99b4
Sinmiloluwa <mofeoluwae@gmail.com>
2025-03-12 18:49:25 +0100

    initial commit for grocery app

---
+++ lib/screens/onboarding.dart
@@ -0,0 +1,181 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+class Onboarding extends StatefulWidget {
+  const Onboarding({super.key});
+
+  @override
+  State<Onboarding> createState() => _OnboardingState();
+}
+
+class _OnboardingState extends State<Onboarding> {
+  final PageController _pageController = PageController();
+  int _currentPage = 0;
+  final int _numPages = 3;
+
+  List<Map<String, dynamic>> onboardingData = [
+    {
+      'title': 'Start Your Journey with Grocery',
+      'description': 'Discover a wide range of fresh, high-quality products.',
+      'image': 'assets/images/groceries.png',
+    },
+    {
+      'title': 'Fast Delivery',
+      'description': 'Get your groceries delivered quickly and efficiently.',
+      'image': 'assets/images/groceries2.jpg',
+    },
+    {
+      'title': 'Easy Payment',
+      'description': 'Multiple payment options available.',
+      'image': 'assets/images/groceries3.jpg',
+    },
+  ];
+
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: SafeArea(
+        child: Column(
+          children: [
+            // const SizedBox(height: 50,),
+            Expanded(
+              flex: 6,
+              child: PageView.builder(
+                controller: _pageController,
+                onPageChanged: (int page) {
+                  setState(() {
+                    _currentPage = page;
+                  });
+                },
+                itemCount: _numPages,
+                itemBuilder: (context, index) {
+                  return buildOnboardingPage(
+                    title: onboardingData[index]['title']!,
+                    description: onboardingData[index]['description']!,
+                    image: onboardingData[index]['image']!,
+                  );
+                },
+              ),
+            ),
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 20),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: List.generate(
+                        _numPages,
+                            (index) => buildDotIndicator(index),
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        if (_currentPage < _numPages - 1) {
+                          _pageController.nextPage(
+                            duration: const Duration(milliseconds: 300),
+                            curve: Curves.easeIn,
+                          );
+                        } else {
+                          Navigator.pushReplacement(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => const HomeScreen(),
+                            ),
+                          );
+                        }
+                      },
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: const Color(0xFF2FAA7A),
+                        minimumSize: const Size(100, 50),
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10),
+                        ),
+                      ),
+                      child: Text(
+                        _currentPage < _numPages - 1 ? 'Next' : 'Get Started',
+                        style: const TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          color: Colors.white,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+  Widget buildOnboardingPage({
+    required String title,
+    required String description,
+    required String image,
+  }) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            flex: 3,
+            child: Container(
+              // decoration: BoxDecoration(
+              //   // color: const Color(0xFFE8F5F0), // Light mint background similar to image
+              //   // borderRadius: BorderRadius.circular(20),
+              // ),
+              child: Center(
+                child: Image.asset(
+                  image,
+                  fit: BoxFit.contain,
+                ),
+              ),
+            ),
+          ),
+          const SizedBox(height: 15),
+          Text(
+            title,
+            style: const TextStyle(
+              fontFamily: 'Manrope',
+              fontSize: 32,
+              fontWeight: FontWeight.bold,
+              letterSpacing: 0.5,
+              color: Colors.black87,
+            ),
+            textAlign: TextAlign.left,
+          ),
+          Text(
+            description,
+            style: const TextStyle(
+              fontFamily: 'Manrope',
+              fontSize: 20,
+              color: Colors.black54,
+            ),
+            textAlign: TextAlign.left,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildDotIndicator(int index) {
+    return Container(
+      margin: const EdgeInsets.symmetric(horizontal: 5),
+      height: 10,
+      width: _currentPage == index ? 25 : 10,
+      decoration: BoxDecoration(
+        color: _currentPage == index
+            ? const Color(0xFF2FAA7A)
+            : const Color(0xFFD8D8D8),
+        borderRadius: BorderRadius.circular(5),
+      ),
+    );
+  }
+}
